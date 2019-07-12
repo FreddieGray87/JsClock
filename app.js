b9c5d51927e5d9f85204b3f565adc9a6bb65d190
Fred Graybeal <fred.c.graybeal@gmail.com>
2019-07-12 13:21:46 -0400

    finished project

--- app.js
+++ app.js
@@ -32,8 +32,25 @@ function currentTime() {
     let hour = date.getHours();
     let min = date.getMinutes();
     let sec = date.getSeconds();
+    let midday = "AM";
+    midday = (hour >= 12) ? "PM" : "AM";
+    hour = (hour == 0) ? 12 : ((hour > 12) ? (hour - 12) : hour);
+    hour = updateTime(hour);
+    min = updateTime(min);
+    sec = updateTime(sec);
+    document.getElementById('clock').textContent = `${hour}:${min}:${sec}`;
+    let t = setTimeout(function () {
+        currentTime();
+    }, 1000);
 }
 
+function updateTime(k) {
+    if (k < 10) {
+        return '0' + k;
+    } else {
+        return k;
+    }
 
+}
 
-currentTime(date);
+currentTime();
